6831c1813e7fd61b42f84c38d60ce27235604e0d
khadidja kalkoul <khadidja@air-de-khadidja.home>
2022-01-28 16:55:05 +0100

    edit

--- starterOnly/script/modal.js
+++ starterOnly/script/modal.js
@@ -5,7 +5,7 @@ navBtn.addEventListener("click", editNav);
 function editNav() {
   var x = document.getElementById("myTopnav");
   if (x.className === "topnav") {
-    x.className += "responsive";
+    x.className += " responsive";
   } else {
     x.className = "topnav";
   }
